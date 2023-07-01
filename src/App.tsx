d1972e22423d6da247c415ee58555387166b173e
nyominkhat <108140828+nyominkhat@users.noreply.github.com>
2023-07-02 00:30:12 +0630

    Pagination progress

--- src/App.tsx
+++ src/App.tsx
@@ -54,99 +54,116 @@ function App() {
     setStep((prevState) => (prevState === 2 ? 2 : prevState + 1));
   };
 
+  const getWidth = () => {
+    return `${(step + 1) * 33.33}%`;
+  };
+
   return (
-    <form
-      onSubmit={onSubmit}
-      className="w-screen h-screen overflow-hidden bg-[#EEE2D7]"
-    >
-      <div
-        className="flex w-full h-full transition-transform duration-300 ease-out"
-        style={{ transform: `translateX(-${step * 100}%)` }}
+    <>
+      <div className="fixed left-0 w-screen px-1 top-10">
+        <p className="mx-5 mb-2 text-base font-semibold text-red-600">
+          {step + 1} of 3
+        </p>
+        <div className=" bg-[#D6CBC1] rounded-full">
+          <div
+            style={{ width: getWidth() }}
+            className="h-1.5 transition-all duration-500 bg-red-300 border border-red-300 rounded-md"
+          ></div>
+        </div>
+      </div>
+      <form
+        onSubmit={onSubmit}
+        className="w-screen h-screen overflow-hidden bg-[#EEE2D7]"
       >
-        <section className="flex flex-col items-center justify-center flex-shrink-0 w-full h-full gap-4">
-          <div className="flex flex-col gap-2">
-            <label htmlFor="name" className="text-sm font-semibold">
-              Username
-            </label>
-            <input
-              className="px-3 py-2 rounded-md outline-none"
-              id="name"
-              type="text"
-              {...register("name")}
-            />
-          </div>
-          {errors.name && (
-            <p className="text-sm text-red-500">{errors.name.message}</p>
-          )}
-        </section>
+        <div
+          className="flex w-full h-full transition-transform duration-300 ease-out"
+          style={{ transform: `translateX(-${step * 100}%)` }}
+        >
+          <section className="flex flex-col items-center justify-center flex-shrink-0 w-full h-full gap-4">
+            <div className="flex flex-col gap-2">
+              <label htmlFor="name" className="text-sm font-semibold">
+                Username
+              </label>
+              <input
+                className="px-3 py-2 rounded-md outline-none"
+                id="name"
+                type="text"
+                {...register("name")}
+              />
+            </div>
+            {errors.name && (
+              <p className="text-sm text-red-500">{errors.name.message}</p>
+            )}
+          </section>
 
-        <section className="flex flex-col items-center justify-center flex-shrink-0 w-full h-full gap-4">
-          <div className="flex flex-col gap-2">
-            <label htmlFor="password" className="text-sm font-semibold">
-              Type password
-            </label>
-            <input
-              className="px-3 py-2 rounded-md outline-none"
-              id="password"
-              type="password"
-              {...register("password")}
-            />
-          </div>
-          {errors.password && (
-            <p className="text-sm text-red-500">{errors.password.message}</p>
-          )}
-        </section>
+          <section className="flex flex-col items-center justify-center flex-shrink-0 w-full h-full gap-4">
+            <div className="flex flex-col gap-2">
+              <label htmlFor="password" className="text-sm font-semibold">
+                Type password
+              </label>
+              <input
+                className="px-3 py-2 rounded-md outline-none"
+                id="password"
+                type="password"
+                {...register("password")}
+              />
+            </div>
+            {errors.password && (
+              <p className="text-sm text-red-500">{errors.password.message}</p>
+            )}
+          </section>
 
-        <section className="flex flex-col items-center justify-center flex-shrink-0 w-full h-full gap-4">
-          <div className="flex flex-col gap-2">
-            <label htmlFor="address" className="text-sm font-semibold">
-              Type an address
-            </label>
-            <input
-              className="px-3 py-2 rounded-md outline-none"
-              id="address"
-              type="text"
-              {...register("address")}
-            />
-          </div>
-          {errors.address && (
-            <p className="text-sm text-red-500">{errors.address.message}</p>
-          )}
+          <section className="flex flex-col items-center justify-center flex-shrink-0 w-full h-full gap-4">
+            <div className="flex flex-col gap-2">
+              <label htmlFor="address" className="text-sm font-semibold">
+                Type an address
+              </label>
+              <input
+                className="px-3 py-2 rounded-md outline-none"
+                id="address"
+                type="text"
+                {...register("address")}
+              />
+            </div>
+            {errors.address && (
+              <p className="text-sm text-red-500">{errors.address.message}</p>
+            )}
 
+            <button
+              className="px-4 py-2 font-semibold text-white transition bg-black border border-white rounded-md hover:bg-black/80"
+              type="submit"
+            >
+              Save
+            </button>
+          </section>
+        </div>
+
+        <div className="fixed gap-4 top-[50%] -translate-y-1/2 flex right-60">
           <button
-            className="px-4 py-2 font-semibold text-white transition bg-black border border-white rounded-md hover:bg-black/80"
-            type="submit"
+            onClick={prev}
+            type="button"
+            className={`px-4 py-2 font-semibold text-white transition bg-black border duration-300 border-white rounded-md ${
+              step === 0
+                ? "bg-[#c1b7ad] cursor-auto text-gray-500"
+                : "hover:bg-black/80"
+            }`}
           >
-            Save
+            prev
           </button>
-        </section>
-      </div>
-
-      <div className="fixed gap-4 top-[50%] -translate-y-1/2 flex right-60">
-        <button
-          onClick={prev}
-          type="button"
-          className={`px-4 py-2 font-semibold text-white transition bg-black border duration-300 border-white rounded-md ${
-            step === 0
-              ? "bg-[#D1C7BD] cursor-auto text-gray-500"
-              : "hover:bg-black/80"
-          }`}
-        >
-          prev
-        </button>
-        <button
-          onClick={next}
-          type="button"
-          className={`px-4 py-2 font-semibold text-white transition bg-black border duration-300 border-white rounded-md ${
-            step === 2
-              ? "bg-[#D1C7BD] cursor-auto text-gray-500"
-              : "hover:bg-black/80"
-          }`}
-        >
-          next
-        </button>
-      </div>
-    </form>
+          <button
+            onClick={next}
+            type="button"
+            className={`px-4 py-2 font-semibold text-white transition bg-black border duration-300 border-white rounded-md ${
+              step === 2
+                ? "bg-[#c1b7ad] cursor-auto text-gray-500"
+                : "hover:bg-black/80"
+            }`}
+          >
+            next
+          </button>
+        </div>
+      </form>
+    </>
   );
 }
 
